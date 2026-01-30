8f4b5c61cf59581cf28d1f510e6fdd9cc7eb7726
Ricardo Cuéllar <rcuellarsn@gmail.com>
2026-01-30 02:14:00 -0600

    Add maps and structs code

--- 04-Tipos compuestos/main.go
+++ 04-Tipos compuestos/main.go
@@ -1,5 +1,21 @@
 package main
 
+//Crear dos structs
+
+//Item -> SKU, Name, Price, Qty
+//Order -> ID, Customer, Items, Meta
+
+/*
+	1. Asignar valores al order
+	2. Imprimir
+	   - Order ID
+	   - Cliente
+	   - Acceso al primer elemento del item
+	   - Acceso directo a un valor en el map de Meta.
+	   - Modificar el cupon a HOLACARADEBOLA
+	   - Mostrar el cupon modificado
+*/
+
 func main() {
 
 }
