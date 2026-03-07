89d963f4f8d89bcc0eb896fcf2cfd8d577735546
Ricardo Cuéllar <rcuellarsn@gmail.com>
2026-03-06 18:13:08 -0600

    Add package import

--- 12-Modulos y paquetes/greetctl/cmd/greetctl/main.go
+++ 12-Modulos y paquetes/greetctl/cmd/greetctl/main.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/ricardocuellar/greetctl/pkg/greet"
+)
 
 func main() {
+	msg := greet.Hello("Fernando")
 	fmt.Println("Proyecto listo desde CMD!")
+	fmt.Println(msg)
 }
---
+++ 12-Modulos y paquetes/greetctl/pkg/greet/greet.go
@@ -0,0 +1,5 @@
+package greet
+
+func Hello(name string) string {
+	return "Hola, " + name + " :)"
+}
